4bc86984eb744e6b0a34a43146bda98dd6951a43
Ajda Zavrtanik Drglin <ajda.zavrtanik@gmail.com>
2016-07-05 10:58:25 +0200

    shiny naprej

--- shiny/server.R
+++ shiny/server.R
@@ -11,9 +11,34 @@ shinyServer(function(input, output) {
   conn <- src_postgres(dbname = db, host = host,
                        user = user, password = password)
   tbl.plezalisca <- tbl(conn, "plezalisca")
-  output$plezalisca <- renderTable({
+  tbl.plezaliscastevilo <- tbl(conn, sql("SELECT nekoime.plezalisce, drzava, stevilo FROM nekoime JOIN plezalisca ON nekoime.plezalisce=plezalisca.plezalisce ORDER BY drzava, plezalisce"))
+  tbl.drzave <- tbl(conn, "drzave")
+  tbl.smeri <- tbl(conn, "smeri")
+  tbl.drzavestevilo <-tbl(conn, sql("SELECT drzava, count(*) AS stevilo_plezalisc FROM plezalisca GROUP BY drzava ORDER BY stevilo_plezalisc DESC"))
+  
+  ############################
+  
+  output$plezaliscaPoDrzavah <- renderTable({
     
-    t <- tbl.plezalisca
+    t <- tbl.plezaliscastevilo %>% if(input$izberi_drzavo=="vse države") filter(input$stevilo_smeri[1]<= stevilo & stevilo <= input$stevilo_smeri[2]) else filter(drzava==input$izberi_drzavo & input$stevilo_smeri[1]<= stevilo & stevilo <= input$stevilo_smeri[2]) %>% data.frame()
     t
   })
+  
+  ##################
+  output$drzavestevilo <- renderTable({drz <- tbl.drzavestevilo %>% filter(input$st_plezalisc[1] <= stevilo_plezalisc & stevilo_plezalisc<= input$st_plezalisc[2]) %>% data.frame()
+                            drz})
+  
+  #######################
+  
+  output$plezaliscastevilo <- renderTable({ps <- tbl.plezaliscastevilo %>% data.frame()
+  ps
+    
+    
   })
+  
+  
+  
+  
+  
+  })
+
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,8 +1,64 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Države"),
- 
-  tableOutput("plezalisca")
+  titlePanel("Plezališča"),
+  
+  tabsetPanel(
     
-  ))
+  #############################
+    
+  tabPanel( "Plezališča po državah",
+    sidebarLayout(
+    sidebarPanel(
+      selectInput("izberi_drzavo",
+                  label = "Izberi državo:",
+                  choices = list("vse države", "Avstrija","Bosna in Hercegovina","Češka","Črna gora","Grčija","Hrvaška", "Juzna Afrika", "Maroko", "Nemčija", "Poljska", "Slovaška", "Slovenija","Španija", "Srbija", "Švedska", "Švica", "Tajska", "Turčija" ,"Združene države Amerike"),
+                  selected="vse države"
+    ),
+    sliderInput("stevilo_smeri",
+                label= "Število smeri v plezališču:",
+                min= 1,
+                max =706,
+                value=c(1, 706)
+      
+    )
+    ),
+    
+    mainPanel(
+  tableOutput("plezaliscaPoDrzavah")
+))),
+##############################
+tabPanel( "Države",
+          sidebarLayout(
+            sidebarPanel(
+              sliderInput("st_plezalisc",
+                          label = "Število plezališč:",
+                          min = 1,
+                          max = 103,
+                          value = c(0,103)
+              )),
+            
+            mainPanel(
+              tableOutput("drzavestevilo")
+            ))),
+
+##############################
+tabPanel( "neki",
+          sidebarLayout(
+            sidebarPanel(
+              sliderInput("st_plezalisc",
+                          label = "Število plezališč:",
+                          min = 1,
+                          max = 103,
+                          value = c(0,103)
+              )
+              
+              ),
+            
+            mainPanel(
+              tableOutput("plezaliscastevilo")
+            )))
+
+
+
+  )))
